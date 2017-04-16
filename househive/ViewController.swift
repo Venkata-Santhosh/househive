cb249c159228addf073ee24e74dbe0fe5d46af7d
Piduri Venkata Santhosh <VenkataSanthosh@piduris-macbook-pro.local>
2017-04-16 17:14:28 -0400

    Initial Commit

---
+++ househive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  househive
+//
+//  Created by Piduri Venkata Santhosh on 4/16/17.
+//  Copyright © 2017 Piduri Venkata Santhosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
