4f9330291815058337a7b854c79a027680e2529e
Piduri Venkata Santhosh <VenkataSanthosh@Piduris-MacBook-Pro.local>
2017-04-16 21:20:40 -0400

    Signup ux design and added seque

---
+++ househive/SignUpViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpViewController.swift
+//  househive
+//
+//  Created by Piduri Venkata Santhosh on 4/16/17.
+//  Copyright © 2017 Piduri Venkata Santhosh. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
